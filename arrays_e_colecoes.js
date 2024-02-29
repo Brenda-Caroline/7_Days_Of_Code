93749b6c20ca7f68b4e2eca89fc87567734cc250
Brenda-Caroline <2014brendacs0@gmail.com>
2024-02-29 14:53:21 -0300

    Adiciona valores nos arrays correspondentes ao grupo

--- arrays_e_colecoes.js
+++ arrays_e_colecoes.js
@@ -7,12 +7,37 @@ let doces= [];
 let produtosCasa = [];
 let higiene = [];
 
-const adicionar = receber("Deseja adicionar um produto na lista de compras? Responda com 'sim ou não' ");
-if (adicionar == "sim")
+let adicionar = receber("Deseja adicionar um produto na lista de compras? Responda com 'sim ou não' ");
+while (adicionar == "sim")
 {
-    let comida = receber("Qual? ");
-    let grupo = receber("Em qual grupo o produto se encaixa? Digite o número respectivo ao grupo: \n1 - frutas\n2 -laticinios\n3 - congelados\n4 - doces\n5 - produtos de casa\n6 - higiene pessoal ");
+    let produto = receber("Qual? ");
+    let grupo = receber("Em qual grupo o produto se encaixa? Digite o número respectivo ao grupo: \n1 - frutas\n2 - laticinios\n3 - congelados\n4 - doces\n5 - produtos de casa\n6 - higiene pessoal\n ");
 
-    
+    switch (grupo)
+    {
+        case '1':
+            frutas.push(produto);
+            break;
+        case '2':
+            laticinios.push(produto);
+            break;
+        case '3':
+            congelados.push(produto);
+            break;
+        case '4':
+            doces.push(produto);
+            break;
+        case '5': 
+            produtosCasa.push(produto);
+            break;
+        case '6':
+            higiene.push(produto);
+            break;
+    }
+    adicionar = receber("Deseja adicionar um produto na lista de compras? Responda com 'sim ou nao' ");
+}
 
+if (adicionar == "nao")
+{
+    console.log(`Lista de Compras:\n Frutas: ${frutas}\n Laticínios: ${laticinios}\n Congelados: ${congelados}\n Doces: ${doces}\n Produtos de casa: ${produtosCasa}\n Higiene pessoal: ${higiene}`);
 }
