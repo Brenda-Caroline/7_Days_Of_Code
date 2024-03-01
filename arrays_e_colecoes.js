9bd9a70665e82a2d6c4255b628d44ea1176c9d85
Brenda-Caroline <2014brendacs0@gmail.com>
2024-03-01 16:15:20 -0300

    Adiciona possibilidade de remover produto

--- arrays_e_colecoes.js
+++ arrays_e_colecoes.js
@@ -7,37 +7,51 @@ let doces= [];
 let produtosCasa = [];
 let higiene = [];
 
-let adicionar = receber("Deseja adicionar um produto na lista de compras? Responda com 'sim ou não' ");
-while (adicionar == "sim")
+let adicionarOuRemover = receber("Deseja adicionar ou remover um produto da lista de compras? ");
+if(adicionarOuRemover == "adicionar")
 {
-    let produto = receber("Qual? ");
-    let grupo = receber("Em qual grupo o produto se encaixa? Digite o número respectivo ao grupo: \n1 - frutas\n2 - laticinios\n3 - congelados\n4 - doces\n5 - produtos de casa\n6 - higiene pessoal\n ");
-
-    switch (grupo)
+    adicionar = receber("Tem certeza que deseja adicionar um novo produto? Responda com 'sim' ou 'nao'")
+    while (adicionar == "sim")
     {
-        case '1':
-            frutas.push(produto);
-            break;
-        case '2':
-            laticinios.push(produto);
-            break;
-        case '3':
-            congelados.push(produto);
-            break;
-        case '4':
-            doces.push(produto);
-            break;
-        case '5': 
-            produtosCasa.push(produto);
-            break;
-        case '6':
-            higiene.push(produto);
-            break;
+        let produto = receber("Qual? ");
+        let grupo = receber("Em qual grupo o produto se encaixa? Digite o número respectivo ao grupo: \n1 - frutas\n2 - laticinios\n3 - congelados\n4 - doces\n5 - produtos de casa\n6 - higiene pessoal\n ");
+
+        switch (grupo)
+        {
+            case '1':
+                frutas.push(produto);
+                break;
+            case '2':
+                laticinios.push(produto);
+                break;
+            case '3':
+                congelados.push(produto);
+                break;
+            case '4':
+                doces.push(produto);
+                break;
+            case '5': 
+                produtosCasa.push(produto);
+                break;
+            case '6':
+                higiene.push(produto);
+                break;
+        }
+            adicionar = receber("Deseja adicionar um produto na lista de compras? Responda com 'sim ou nao' ");
     }
-    adicionar = receber("Deseja adicionar um produto na lista de compras? Responda com 'sim ou nao' ");
 }
 
 if (adicionar == "nao")
 {
     console.log(`Lista de Compras:\n Frutas: ${frutas}\n Laticínios: ${laticinios}\n Congelados: ${congelados}\n Doces: ${doces}\n Produtos de casa: ${produtosCasa}\n Higiene pessoal: ${higiene}`);
+}
+
+if(adicionarOuRemover == "remover")
+{
+    console.log(`Lista de Compras:\n Frutas: ${frutas}\n Laticínios: ${laticinios}\n Congelados: ${congelados}\n Doces: ${doces}\n Produtos de casa: ${produtosCasa}\n Higiene pessoal: ${higiene}`);
+    remover = receber("Qual produto deseja remover?");
+    if (frutas.includes(remover) || laticinios.includes(remover) || congelados.includes(remover) || doces.includes(remover) || produtosCasa.includes(remover) || higiene.includes(remover))
+    {
+        console.log("Encontrei");
+    }
 }
