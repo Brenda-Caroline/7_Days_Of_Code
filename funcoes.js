13d18cb3b474ced6426ef1e9cfc10e2434dd2048
Brenda-Caroline <2014brendacs0@gmail.com>
2024-03-05 17:28:38 -0300

    Cria variaveis de numeros, switch, e transforma as entradas em números

--- funcoes.js
+++ funcoes.js
@@ -1,21 +1,54 @@
 // Criando uma calculadora
 
+const receber = require('prompt-sync')({sigint:true});
+
 function soma (numero1, numero2)
 {
-    return numero1 + numero2;
+    return Number(numero1) + Number(numero2);
 }
 
 function subtracao (numero1, numero2)
 {
-    return numero1 - numero2;
+    return Number(numero1) - Number(numero2);
 }
 
 function multiplicacao (numero1, numero2)
 {
-    return numero1 * numero2;
+    return Number(numero1) * Number(numero2);
 }
 
 function divisao (numero1, numero2)
 {
-    return numero1 / numero2;
+    return Number(numero1) / Number(numero2);
+}
+
+console.log("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair");
+operacao = receber("Digite o número da operação que deseja realizar: ");
+
+let numero1 = receber("Digite o primeiro número: ");
+let numero2 = receber("Digite o segundo número: ");
+
+switch(operacao)
+{
+    case '1':
+        result = soma(numero1, numero2);
+        console.log(result);
+        break;
+    case '2':
+        result = subtracao(numero1, numero2);
+        console.log(result);
+        break;
+    case '3':
+        result = multiplicacao(numero1, numero2);
+        console.log(result);
+        break;
+    case '4':
+        result = divisao(numero1, numero2);
+        console.log(result);
+        break;
+    case '5':
+        console.log("Até a próxima");
+        break;
+    default:
+        console.log("Operação não encontrada");
 }
